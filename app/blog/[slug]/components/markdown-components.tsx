79b68127f68d47af361ee68e8ff825c4f8f4d7bd
Michael Tseng <michael08120@gmail.com>
2024-03-29 15:53:37 -0700

    added code highlight

--- app/blog/[slug]/components/markdown-components.tsx
+++ app/blog/[slug]/components/markdown-components.tsx
@@ -1,6 +1,7 @@
 import Link, {LinkProps} from 'next/link'
 import Image, {ImageProps} from 'next/image'
 import type { MDXComponents } from 'mdx/types'
+import { Code } from 'bright'
 
 export const mdxComponents: MDXComponents = {
   a: ({ children, ...props }) => {
@@ -15,6 +16,7 @@ export const mdxComponents: MDXComponents = {
     // See the details below for my solution.
     return <Image {...(props as ImageProps)} />
   },
+  pre: Code,
   // any other components you want to use in your markdown
 }
 
