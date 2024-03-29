542fbd4b54433a8ef5b97c127263ca990a680f29
Michael Tseng <michael08120@gmail.com>
2024-03-28 21:31:40 -0700

    added some text

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <p>18, building for the singularity</p>
       <p>🌉 love startups, cities, long articles, ocean blue, and night sky</p>
       <br />
-      <p>I grew up in Hsinchu, Taiwan, a city where the wind never stops blowing. The thriving semiconductor industry there inspired me to pursue a path in STEM, so I picked up C++ upon entering high school. I later became interested in machine learning and started working on side projects and conducting research by reading papers and watching tutorials on YouTube. Besides writing code, I sometimes write prose and poetry as a window out of my chaotic life.</p>
+      <p>I grew up in Hsinchu, Taiwan, a city where the wind never stops blowing. The thriving semiconductor industry there inspired me to pursue a path in STEM, so I picked up C++ upon entering high school. I later became interested in machine learning and started working on side projects and doing computer vision research in local university. Besides writing code, I sometimes write prose and poetry as a window out of my chaotic life.</p>
     </div>
   );
 }
