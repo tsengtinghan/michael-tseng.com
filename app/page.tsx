c573af1145f6f4160d4318003455c1fbd5d41ec0
Michael Tseng <michael08120@gmail.com>
2024-04-02 15:52:02 -0700

    update homepage and add typewriter effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Link from "next/link";
 export default function Homepage() {
   return (
-    <></>
+    <div>
+      still searching my{" "}
+      <Link
+        href={"https://www.youtube.com/watch?v=PUv66718DII"}
+        className="underline"
+      >
+        principles
+      </Link>
+      <span className="typewriter"> </span>
+    </div>
   );
 }
