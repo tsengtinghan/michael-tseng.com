8147c5c974aceeff25826aa12c1a15a6dac2ceb6
Michael Tseng <michael08120@gmail.com>
2024-05-23 00:31:57 -0700

    added link to social

--- app/components/social.tsx
+++ app/components/social.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 export default function Social() {
-    return (
-        <div className="flex justify-start space-x-2"> 
-
-            <Image src="/twitter.svg" alt="logo" width={25} height={25} />
-            <Image src="/square-github.svg" alt="logo" width={25} height={25} />
-        </div>
-    );
+  return (
+    <div className="flex justify-start space-x-2">
+      <Link href="https://x.com/michaeltseng_">
+        <Image src="/twitter.svg" alt="logo" width={25} height={25} />
+      </Link>
+      <Link href="https://github.com/tsengtinghan">
+        <Image src="/square-github.svg" alt="logo" width={25} height={25} />
+      </Link>
+    </div>
+  );
 }
