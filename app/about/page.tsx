5aee17b7e33f6beafc66c4263281da5dc12dbb02
Michael Tseng <michael08120@gmail.com>
2024-05-23 00:28:40 -0700

    add social component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,27 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Social from "@components/social";
 export default function About() {
   return (
     <div>
-      <p>19, building for the <Link href={"/blog/singularity-is-near-1"}>singularity</Link></p>
-      <p>@ 🌉 I love startups!
-      <br /> currently learning web dev and ai to build something useful
-      <br /> 
+      <p>
+        19, building for the{" "}
+        <Link href={"/blog/singularity-is-near-1"}>singularity</Link>
+      </p>
+      <p>
+        @ 🌉 I love startups!
+        <br /> currently learning web dev and ai to build something useful
+        <br />
       </p>
       <br />
-      <p>I grew up in Hsinchu, Taiwan, a city where the wind never stops blowing. The thriving semiconductor industry there inspired me to pursue a path in STEM, so I picked up C++ upon entering high school. I later became interested in machine learning and started working on side projects and doing computer vision research in local university. Besides writing code, I sometimes write prose and poetry in Chinese.</p>
+      <p className="pb-8">
+        I grew up in Hsinchu, Taiwan, a city where the wind never stops blowing.
+        The thriving semiconductor industry there inspired me to pursue a path
+        in STEM, so I picked up C++ upon entering high school. I later became
+        interested in machine learning and started working on side projects and
+        doing computer vision research in local university. Besides writing
+        code, I sometimes write prose and poetry in Chinese.
+      </p>
+      <Social />
     </div>
   );
 }
--- app/components/social.tsx
+++ app/components/social.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Social() {
     return (
-        <div className="flex justify-start"> 
+        <div className="flex justify-start space-x-2"> 
+
             <Image src="/twitter.svg" alt="logo" width={25} height={25} />
             <Image src="/square-github.svg" alt="logo" width={25} height={25} />
         </div>
