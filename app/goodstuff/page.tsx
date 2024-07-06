5c03b09aff3594cdce5eb95106e2e70afd3043b1
Michael Tseng <michael08120@gmail.com>
2024-07-05 22:19:08 -0700

    some style

--- app/goodstuff/page.tsx
+++ app/goodstuff/page.tsx
@@ -1,9 +1,16 @@
 import { readPage } from "@lib/notion/read";
+import Link from "next/link";
 export default function GoodStuffPage() {
   readPage();
   return (
-    <div>
-      <h1>good stuff coming soon...</h1>
+    <div className="flex flex-col">
+      <ul className="list-disc list-inside">
+        <li><Link href={"/articles"}> blog posts & articles</Link></li>
+        <li><Link href={"/collections"}> 收藏 </Link></li>
+        <li><Link href={"/quotes"}> quotes </Link></li>
+        <li><Link href={"/books"}> books </Link></li>
+      </ul>
     </div>
   );
+  
 }
