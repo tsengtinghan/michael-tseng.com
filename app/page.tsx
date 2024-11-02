2a4da200e52885d18267b833f94b8ef55f3179f2
Michael Tseng <michael08120@gmail.com>
2024-11-02 00:57:35 -0700

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 export default function Homepage() {
   return (
     <div>
+      building
+      <Link href={"https://ipmakerv2.vercel.app/"}> ipmaker</Link>
+      <br />
+      <br />
       still searching my{" "}
       <Link
         href={"https://www.youtube.com/watch?v=PUv66718DII"}
